9319ec2bc569afb979deb4885aaac8b63578ddf4
Victor <vbaziuk13@gmail.com>
2021-10-29 17:30:59 -0400

    cleaned up

--- src/models/schema.js
+++ src/models/schema.js
@@ -3,10 +3,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const model = new Schema({
-//     name: { type: String, required: true, index: { unique: true } },
-//     password: { type: String, require: true }
-// })
 
 const model = new Schema({
     name: String,
@@ -15,4 +11,3 @@ const model = new Schema({
 });
 
 module.exports = mongoose.model('Model', model);
-// module.exports = mongoose.model('Alt', altModel);
